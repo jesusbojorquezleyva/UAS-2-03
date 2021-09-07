b6540a5ed758e26406d24ced25500e6f5bd5ca4c
jesus eduardo bojorquez leyva <eduardo-131415@hotmail.com>
2021-09-06 20:44:09 -0700

    hola mundo y ejemplos

---
+++ entrad.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+int main ()
+{
+  std :: cout << "introduzca un numero: ";
+  int x {};
+  std :: cin>> x;
+  std :: cout << "ingresaste: " << x << '\n';
+return 0 ;
+}
---
+++ multiplicax2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main ()
+{
+    int x{};
+    std::cout<<"Introduzca un numero: ";
+    std:: cin>> x;
+    std::cout<<"El doble del numero "<<x<<" es: "<<x*2;
+    return 0;
+}
